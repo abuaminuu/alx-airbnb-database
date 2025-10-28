2ffcc0ae26ae76088808c215b922bcc99d1bde25
abuaminuu <43301447+abuaminuu@users.noreply.github.com>
2025-10-28 09:02:34 +0100

    reordered Full Outer Join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,4 +7,4 @@ SELECT * FROM Booking INNER JOIN User ON Booking.user_id = User.user_id LIMIT 10
 SELECT * FROM Property LEFT JOIN Review ON Property.property_id = Review.property_id LIMIT 5;
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
-SELECT * FROM User FULL OUTER JOIN Booking ON Booking.user_id = User.user_id LIMIT 5;
+SELECT * FROM User FULL OUTER JOIN Booking ON User.user_id = Booking.user_id LIMIT 5;
